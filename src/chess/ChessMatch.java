9b35d40cc74702d518ba8cc8758bb422eaea2ba1
wilFarias <wilsonluiz.dsf@gmail.com>
2020-08-25 20:47:21 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,7 +24,13 @@ public class ChessMatch {
 		}
 		return matriz;
 	}
-	
+
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
@@ -33,21 +39,21 @@ public class ChessMatch {
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
 	}
-	
+
 	private Piece makeMove(Position source, Position target) {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		return capturedPiece;
 	}
-	
+
 	private void validateSourcePosition(Position position) {
-		if(!board.thereIsAPiece(position))
+		if (!board.thereIsAPiece(position))
 			throw new ChessException("There is no piece on source position");
-		if(!board.piece(position).isThereAnyPossiblemove())
+		if (!board.piece(position).isThereAnyPossiblemove())
 			throw new ChessException("There is no possible moves for the chosen piece");
 	}
-	
+
 	private void validateTargetPosition(Position source, Position target) {
 		if (!board.piece(source).possibleMove(target)) {
 			throw new ChessException("The chosen piece can't move to target position");
